954b135d88fa0f65dc9616253105316348efcd76
Your Name <your.email@example.com>
2025-10-31 01:10:22 +0000

    t

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 
 # Wait for database to be ready
 echo "Waiting for database to be ready..."
-while ! pg_isready -h db -p 5432 -U ${DB_USERNAME:-api_user} > /dev/null 2>&1; do
+while ! pg_isready -h ${DB_HOST:-db} -p ${DB_PORT:-5432} -U ${DB_USERNAME:-api_user} > /dev/null 2>&1; do
   echo "Database is unavailable - sleeping"
   sleep 2
 done
